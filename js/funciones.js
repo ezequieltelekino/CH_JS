7d326f9a15b6f88f2046c898d784bb79e545e1c1
Ezequiel <ezequieltelekino@gmail.com>
2023-02-22 12:04:51 -0300

    Agrego lógica de logout

--- js/funciones.js
+++ js/funciones.js
@@ -4,15 +4,7 @@ class Civilizacion{
         this.descripcion = descripcion;
     }
 
-    mostrarDatos(){
-        console.log("Mostrando datos de: " + this.nombre)
 
-        // el objeto tiene un campo "descripcion", que a su vez es un array de strings, cada posición es una pantalla de la descripción
-        this.descripcion.forEach((texto, index) => {
-            let textoCompleto = "(" + String(Number(index + 1)) + " de " + this.descripcion.length + ") " + texto ;
-            alert(textoCompleto);
-        });
-    }
 
     mostrarDatosEnHTML(){
         console.log("Mostrando en html datos de: " + this.nombre)
@@ -73,26 +65,62 @@ let civilizaciones = [
         "Bonificación de equipo: los cuarteles funcionan un 20% más rápido. Ideal para caer junto a un celta y romper todo."
         ])
 ];
-    
-function muestraMenu(civilizaciones){
 
-        // creo un mensaje indicativo
-    if (nombre == undefined){
+function bienvenidaAnonimo(){
+    console.log("en bienvenidaAnonimo")
+    let advertenciaAnterior = document.getElementById("advertencia");
+    if (advertenciaAnterior){
+        advertenciaAnterior.remove();
+    }
+
+    if (nombre == undefined ){
         console.log ("el nombre está en blanco")
         let advertencia = document.createElement("p");
         advertencia.id = "advertencia";
         advertencia.innerHTML = "Hola, anónimo... ingresá tu nombre, por favor";
+        advertencia.style.marginLeft = "30%";
+
         document.body.append(advertencia);
         return;
     }
+}
+
+function pideCiv(){
+
+    //verifico si ya había un mensaje, y lo elimino
+    let mensajeAnterior = document.getElementById("selecciona");
+    if (mensajeAnterior)
+        mensajeAnterior.remove()
+
+    if (nombre == undefined)
+        return;
 
     let mensaje = document.createElement("p");
+    mensaje.id = "selecciona"
     mensaje.style.textAlign = "center";
     mensaje.style.width = "30%";
     mensaje.style.backgroundColor = "#c0c0e0";
     mensaje.innerHTML = "Seleccioná tu civilización";
     mensaje.style.marginLeft = "33%" ; 
     document.body.append(mensaje);
+}
+function muestraMenu(civilizaciones){
+
+    // borro cosas previas de la pantalla
+    let datosAnteriores = document.getElementById("datos");
+    if (datosAnteriores)
+        datosAnteriores.remove();
+
+    let seccionAnterior = document.getElementById("menu");
+    if (seccionAnterior)
+        seccionAnterior.remove();
+
+        // creo un mensaje indicativo
+    if (nombre == undefined)
+        return;
+    //verifico si ya había un menú, y lo elimino
+
+    pideCiv();
 
     // creo un section
     let seccionMenu = document.createElement("section");
@@ -102,6 +130,7 @@ function muestraMenu(civilizaciones){
     document.body.append(seccionMenu);
     
     // ajusto el tamaño de la civ para que se muestre, en este caso (que son 4) al 25% c/u
+    console.log (civilizaciones)
     let tamanioMaximo = String(100 / civilizaciones.length);
     civilizaciones.forEach( civ  => {
         console.log ("Mostrando " + civ.nombre);
@@ -121,67 +150,105 @@ function muestraMenu(civilizaciones){
         c.onmouseout = () => { c.style.backgroundColor = "#e0e0e0" };
     })
 
-
     let seccionDatos = document.createElement("section");
     seccionDatos.id = "datos";
-
     document.body.append (seccionDatos);
 }
 
-function preguntaNombre(){
-    let textoQueQuieroMostrar = document.createElement("p");
+function saluda(saludo){
+    console.log("saludando")
+    bienvenidaAnonimo()
 
+
+    let botonLogOut = document.createElement ("button");
+    botonLogOut.innerHTML = "Cambiar Usuario";
+    let divContieneSaludo = document.createElement("div");
+    divContieneSaludo.appendChild(saludo);
+    divContieneSaludo.append (botonLogOut);
+    divContieneSaludo.style.display = "flex"
+    divContieneSaludo.style.justifyContent = "space-between"
+    divContieneSaludo.style.marginLeft = "20%"
+    divContieneSaludo.style.marginRight = "10%"
+    botonLogOut.style.borderRadius = "8px";
+    botonLogOut.style.borderRadius = "8px";
+
+    botonLogOut.onclick = () => {
+        console.log("cerrando sesión");
+        nombreAlmacenado = "";
+        sessionStorage.removeItem("nombreAlmacenado");
+        divContieneSaludo.remove();
+        nombre = undefined;
+        pideCiv();
+        preguntaNombre();
+        muestraMenu(civilizaciones);
+    }
+    document.body.append(divContieneSaludo);
+}
+
+function preguntaNombre(){
+    console.log("preguntando nombre" + nombre)
+    bienvenidaAnonimo();
+
+    let saludo = document.createElement("p");
 
     nombreAlmacenado = sessionStorage.getItem("nombreAlmacenado");
     if (nombreAlmacenado){
         nombre = JSON.parse(nombreAlmacenado).nombre;
         console.log ("El usuario " + nombre  + " ya está almacenado");
-        textoQueQuieroMostrar.innerHTML = "<p>Hola, " + nombre + ".  Gracias por volver!</p>";
-        textoQueQuieroMostrar.style.textAlign  = "center";
-        document.body.append(textoQueQuieroMostrar);
+        saludo.innerHTML = "Hola, " + nombre + ".  Gracias por volver!";
+        saludo.style.textAlign  = "center";
+
         let mensajeQueQuieroBorrar = document.getElementById("advertencia");
         if (mensajeQueQuieroBorrar)
             mensajeQueQuieroBorrar.remove();
 
+        saluda(saludo);
         muestraMenu(civilizaciones)        
         return nombre;
     }
-
+    console.log("no hay nombre almacenado, creo el formulario")
+    bienvenidaAnonimo();
     // Si el usuario no está en storage, lo pregunto
     let formularioNombre = document.createElement("input");
     let botonNombre = document.createElement("input");
     let mensajeSobreElNombre = document.createElement("p");
     
-  //  textoQueQuieroMostrar.innerHTML = "<p>Ingresá tu nombre:</p> ";
     formularioNombre.type = "text" ;
     formularioNombre.id = "nombre";
     botonNombre.type = "submit";
     botonNombre.id = "botonNombre";
-    botonNombre.value ="Siguiente";
-    mensajeSobreElNombre.innerHTML = "<p>El nombre está en blanco!</p>";
+    botonNombre.value ="Ingresa tu nombre";
+    mensajeSobreElNombre.innerHTML = "El nombre está en blanco!";
 
-    
-  //  document.body.append(textoQueQuieroMostrar);
-    document.body.append(formularioNombre);
-    document.body.append(botonNombre);
-    
+    let divQueContieneElFormulario = document.createElement("div");
+    divQueContieneElFormulario.append(formularioNombre);
+    divQueContieneElFormulario.append(botonNombre);
+    divQueContieneElFormulario.style.display = "flex";
+    divQueContieneElFormulario.style.justifyContent = "space-between"
+    divQueContieneElFormulario.style.marginLeft = "30%";
+    divQueContieneElFormulario.style.marginRight = "30%";
+
+    document.body.append(divQueContieneElFormulario);
     let boton = document.getElementById("botonNombre");
     boton.onclick = () => {
-        console.log("hizo clic");
         let nombreObtenido = document.getElementById("nombre");        
     
         if (nombreObtenido == null || nombreObtenido.value == "" ){
+            mensajeSobreElNombre.style.marginLeft = "30%";
             document.body.append(mensajeSobreElNombre);
             console.log ("nombre en blanco");
         }
         else{
+            bienvenidaAnonimo();
             mensajeSobreElNombre.remove();
-            textoQueQuieroMostrar.remove();
+            saludo.remove();
+
             formularioNombre.remove();
             botonNombre.remove();
-            textoQueQuieroMostrar.innerHTML = "<p>Hola, " + nombreObtenido.value + ". Ojalá disfrutes del sitio, pese a su estética noventosa! (como el Age, claro)</p>";
-            textoQueQuieroMostrar.style.textAlign = "center";
-            document.body.append(textoQueQuieroMostrar);
+
+            saludo.innerHTML = "Hola, " + nombreObtenido.value + ". Ojalá disfrutes del sitio, pese a su estética noventosa! (como el Age, claro)";
+            saluda(saludo);
+
 
             console.log ("nombre que voy a devolver: " + nombreObtenido.value);
             nombre = nombreObtenido.value;
@@ -199,17 +266,26 @@ function preguntaNombre(){
     } ;
 }
 
+function creaHead(texto){
+    document.body.style.backgroundColor = "#f0e8f0";
+    let head1 = document.createElement("h1");
+    head1.innerHTML = texto;
+    head1.style.textAlign = "center";
+    document.body.append(head1);
+}
+
 // Programa principal
-document.body.style.backgroundColor = "#f0e8f0";
-let head1 = document.createElement("h1");
-head1.innerHTML = "Machete de Age of Empires 2, tercera entrega";
-head1.style.textAlign = "center";
-document.body.append(head1);
-let nombre = undefined;    
+creaHead("Machete de Age of Empires 2, entrega final");
+
 // muestro el menú ANTES que preguntar el nombre, así sé si debo mostrarlo... y mando advertencia.
 // En caso de no mostrar nada, preguntaNombre lo lanza de nuevo ^_^ 
+
+let nombre = undefined;    
+nombre = preguntaNombre() 
+
 muestraMenu(civilizaciones);
 
-nombre = preguntaNombre() 
+
+
 
 
